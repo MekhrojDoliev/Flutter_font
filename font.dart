0f9661ef6731cf5b30f8cb0291aeed9336533a4b
Mekhroj Doliev <101738318+MekhrojDoliev@users.noreply.github.com>
2022-07-21 02:00:23 +0200

    Create font.dart

---
+++ font.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(fontFamily: 'IndieFlower'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Adding Assets'),
+        ),
+        body: Center(
+          child: Stack(
+            fit: StackFit.expand,
+            children: <Widget>[
+              Image(
+                image: AssetImage('assets/images/bg.jpg'),
+              ),
+              Image.asset('assets/icons/icon.png'),
+              Positioned(
+                top:16,
+                left:115,
+                child: Text('My custom font',
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                   fontFamily: 'IndieFlower',
+                ),
+                ),
+                 )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
